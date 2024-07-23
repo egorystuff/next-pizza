ed003cccef824ea10cb04063b68da045e3b42853
Yahor Yankavets <mr.yankovec@gmail.com>
2024-07-23 23:26:39 +0300

    created next app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// 00.31.59.
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Button variant='outline'>Click</Button>
+    </>
+  );
+}
