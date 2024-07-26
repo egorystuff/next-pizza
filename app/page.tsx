886735ea333edb29d327a4cbe4a89f715e1155dc
Yahor Yankavets <mr.yankovec@gmail.com>
2024-07-26 23:57:38 +0300

    added categories and updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
 // 00.59.00.
 
-import { Categories, Container, Title } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className='mt-10 '>
         <Title text='Все пиццы' size='lg' className='font-extrabold' />
+      </Container>
 
-        <Categories />
+      <TopBar />
+
+      <Container className='pb-14'>
+        <div className='flex gap-[60px]'>
+          <div className='w-[250px]'>
+            <Filters />
+          </div>
+
+          <div className='flex-1'>
+            <div className='flex flex-col gap-16'>Product list</div>
+          </div>
+        </div>
       </Container>
     </>
   );
---
+++ components/shared/filters.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return <div>filters</div>;
+};
